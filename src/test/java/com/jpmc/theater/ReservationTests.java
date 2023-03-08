04427cf1b4e8d96cbccbe33c7f0d64a108b3a0a3
Brian Lutz <brian.lutz9@icloud.com>
2023-03-08 15:17:35 -0800

    modify testTotalFeeWithMatineeDiscount and add testTotalFeeWithDayDiscount in ReservationTests

--- src/test/java/com/jpmc/theater/ReservationTests.java
+++ src/test/java/com/jpmc/theater/ReservationTests.java
@@ -11,70 +11,87 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ReservationTests {
-    @Test
-    void testCalculateFee() {
-        var customer = new Customer("John Doe");
-        var showing = new Showing(
-                new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90), 12.5, false),
-                LocalDateTime.now()
-        );
-        var reservation = new Reservation(customer, showing, 3, 3);
-        assertEquals(37.5, reservation.totalFee());
-    }
-    @Test
-    void testCalculateFeeWithFirstSequenceDiscount() {
-        var customer = new Customer("John Doe");
-        var showing = new Showing(
-                new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90), 12.5, true),
-                LocalDateTime.now()
-        );
-        var reservation = new Reservation(customer, showing, 3, 1);
-        assertEquals(28.5, reservation.totalFee());
-    }
 
-    @Test
-    void testCalculateFeeWithSpecialDiscount() {
-        var customer = new Customer("John Doe");
-        var showing = new Showing(
-                new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90), 15.5, true),
-                LocalDateTime.now()
-        );
-        var reservation = new Reservation(customer, showing, 3, 1);
-        assertEquals(37.2, reservation.totalFee());
-    }
+  @Test
+  void testTotalFee() {
+    var customer = new Customer("John Doe");
+    var showing = new Showing(
+        new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90), 12.5, false),
+        LocalDateTime.of(2023, 1, 1, 0, 0)
+    );
+    var reservation = new Reservation(customer, showing, 3, 3);
+    assertEquals(37.5, reservation.totalFee());
+  }
 
-    @Test
-    void testCalculateFeeWithSecondSequenceDiscount() {
-        var customer = new Customer("John Doe");
-        var showing = new Showing(
-                new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90), 12.5, false),
-                LocalDateTime.now()
-        );
-        var reservation = new Reservation(customer, showing, 3, 2);
-        assertEquals(31.5, reservation.totalFee());
-    }
-    @Test
-    void testReservation() {
-        var audienceCount = 3;
-        var customer = new Customer("John Doe");
-        var showing = new Showing(
-                new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90), 12.5, true),
-                LocalDateTime.now()
-        );
-        var reservation = new Reservation(customer, showing, audienceCount, 1);
-        assertEquals(customer, reservation.getCustomer());
-        assertEquals(showing, reservation.getShowing());
-        assertEquals(audienceCount, reservation.getAudienceCount());
-    }
+  @Test
+  void testTotalFeeWithFirstSequenceDiscount() {
+    var customer = new Customer("John Doe");
+    var showing = new Showing(
+        new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90), 12.5, true),
+        LocalDateTime.of(2023, 1, 1, 0, 0)
+    );
+    var reservation = new Reservation(customer, showing, 3, 1);
+    assertEquals(28.5, reservation.totalFee());
+  }
 
-    @ParameterizedTest
-    @ValueSource(ints = {-1, 0})
-    void testReservationWithAudienceCountLessThanOne(int audienceCount) {
-        var customer = new Customer("John Doe");
-        var showing = new Showing(
-                new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90), 12.5, true),
-                LocalDateTime.now()
-        );
-        assertThrows(IllegalArgumentException.class, () -> new Reservation(customer, showing, audienceCount, 1));
-    }
+  @Test
+  void testTotalFeeWithSecondSequenceDiscount() {
+    var customer = new Customer("John Doe");
+    var showing = new Showing(
+        new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90), 12.5, false),
+        LocalDateTime.of(2023, 1, 1, 0, 0)
+    );
+    var reservation = new Reservation(customer, showing, 3, 2);
+    assertEquals(31.5, reservation.totalFee());
+  }
+
+  @Test
+  void testTotalFeeWithSpecialDiscount() {
+    var customer = new Customer("John Doe");
+    var showing = new Showing(
+        new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90), 15.5, true),
+        LocalDateTime.of(2023, 1, 1, 0, 0)
+    );
+    var reservation = new Reservation(customer, showing, 3, 1);
+    assertEquals(37.2, reservation.totalFee());
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {11, 12, 16})
+  void testTotalFeeWithMatineeDiscount(int hour) {
+    var customer = new Customer("John Doe");
+    var showing = new Showing(
+        new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90), 15.5, true),
+        LocalDateTime.of(2023, 1, 1, hour, 0)
+    );
+    var reservation = new Reservation(customer, showing, 3, 1);
+    assertEquals(34.875, reservation.totalFee());
+  }
+
+
+  @Test
+  void testReservation() {
+    var audienceCount = 3;
+    var customer = new Customer("John Doe");
+    var showing = new Showing(
+        new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90), 12.5, true),
+        LocalDateTime.of(2023, 1, 1, 0, 0)
+    );
+    var reservation = new Reservation(customer, showing, audienceCount, 1);
+    assertEquals(customer, reservation.getCustomer());
+    assertEquals(showing, reservation.getShowing());
+    assertEquals(audienceCount, reservation.getAudienceCount());
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {-1, 0})
+  void testReservationWithAudienceCountLessThanOne(int audienceCount) {
+    var customer = new Customer("John Doe");
+    var showing = new Showing(
+        new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90), 12.5, true),
+        LocalDateTime.of(2023, 1, 1, 0, 0)
+    );
+    assertThrows(IllegalArgumentException.class,
+        () -> new Reservation(customer, showing, audienceCount, 1));
+  }
 }
